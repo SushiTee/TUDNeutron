2e4151df25534ef05636c77227cc1a2e679e4903
Sushi Fish <sushi@sushitee.de>
2020-02-07 14:50:47 +0100

    updated device defines for server

--- cpp/simpleneutron/components/hwdevice/hwdevice.h
+++ cpp/simpleneutron/components/hwdevice/hwdevice.h
@@ -10,7 +10,7 @@ typedef struct {
 
 constexpr uint32_t DMA_SIZE = 0x800000u; // total size / dma count / 4
 
-constexpr DMA_DEVICE DMAS[8] = {
+constexpr DMA_DEVICE DMAS[8] = { // complete JA port on Zedboard
     {0x40400000u, 0x10000000u, "/dev/uio0"},
     {0x40410000u, 0x12000000u, "/dev/uio1"},
     {0x40420000u, 0x14000000u, "/dev/uio2"},
@@ -21,7 +21,21 @@ constexpr DMA_DEVICE DMAS[8] = {
     {0x40470000u, 0x1e000000u, "/dev/uio7"}
 };
 
-constexpr uint32_t GPIO_0 = 0x41200000u;
-constexpr uint32_t GPIO_1 = 0x41210000u;
+constexpr const char* FIFO_INTERRUPTS[8] = {
+    "/dev/uio8",
+    "/dev/uio9",
+    "/dev/uio10",
+    "/dev/uio11",
+    "/dev/uio12",
+    "/dev/uio13",
+    "/dev/uio14",
+    "/dev/uio15"
+};
+
+constexpr uint32_t GPIO_0 = 0x41200000u; // dma enable (out)
+constexpr uint32_t GPIO_1 = 0x41210000u; // word length (out)
+constexpr uint32_t GPIO_2 = 0x41220000u; // switch state (in)
+constexpr uint32_t GPIO_3 = 0x41230000u; // enable test generator on input 7 (out)
+constexpr uint32_t GPIO_4 = 0x41240000u; // enable trigger input (JB1 on zedboard) (out)
 
 constexpr uint16_t DEFAULT_WORD_LENGTH = 0x20u;
