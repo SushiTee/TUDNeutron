9bde223f9aa51e98f504ebabf9217c3d4e444522
Sushi Fish <sushi@sushitee.de>
2020-06-03 14:47:15 +0200

    fixed last commit

--- hw/ADCNeutron/src/bd/design_1.tcl
+++ hw/ADCNeutron/src/bd/design_1.tcl
@@ -126,10 +126,6 @@ proc create_root_design { parentCell } {
 
   # Create ports
   set adc_base_clock [ create_bd_port -dir O -type clk adc_base_clock ]
-  set adc_base_port2 [ create_bd_port -dir O -type clk adc_base_port2 ]
-  set_property -dict [ list \
-   CONFIG.FREQ_HZ {10000000} \
- ] $adc_base_port2
   set csa [ create_bd_port -dir O csa ]
   set csb [ create_bd_port -dir O csb ]
   set leds_8bits1 [ create_bd_port -dir O -from 7 -to 0 leds_8bits1 ]
@@ -710,7 +706,7 @@ proc create_root_design { parentCell } {
   connect_bd_net -net normal_1 [get_bd_ports normal] [get_bd_pins spi_programmer_0/normal]
   connect_bd_net -net pattern_1 [get_bd_ports pattern] [get_bd_pins spi_programmer_0/pattern]
   connect_bd_net -net processing_system7_0_FCLK_CLK0 [get_bd_pins adc_signal_detector_0/m00_axis_aclk] [get_bd_pins axi_dma_0/m_axi_s2mm_aclk] [get_bd_pins axi_dma_0/s_axi_lite_aclk] [get_bd_pins axi_mem_intercon/ACLK] [get_bd_pins axi_mem_intercon/M00_ACLK] [get_bd_pins axi_mem_intercon/S00_ACLK] [get_bd_pins fifo_generator_0/s_aclk] [get_bd_pins gpio_enable_splitter/s_axi_aclk] [get_bd_pins gpio_input_trigger/s_axi_aclk] [get_bd_pins gpio_num_words/s_axi_aclk] [get_bd_pins hold_signal_0/m00_axis_aclk] [get_bd_pins processing_system7_0/FCLK_CLK0] [get_bd_pins processing_system7_0/M_AXI_GP0_ACLK] [get_bd_pins processing_system7_0/S_AXI_HP0_ACLK] [get_bd_pins ps7_0_axi_periph/ACLK] [get_bd_pins ps7_0_axi_periph/M00_ACLK] [get_bd_pins ps7_0_axi_periph/M01_ACLK] [get_bd_pins ps7_0_axi_periph/M02_ACLK] [get_bd_pins ps7_0_axi_periph/M03_ACLK] [get_bd_pins ps7_0_axi_periph/S00_ACLK] [get_bd_pins rst_ps7_0_200M/slowest_sync_clk] [get_bd_pins spi_programmer_0/clock]
-  connect_bd_net -net processing_system7_0_FCLK_CLK1 [get_bd_ports adc_base_clock] [get_bd_ports adc_base_port2] [get_bd_pins processing_system7_0/FCLK_CLK1]
+  connect_bd_net -net processing_system7_0_FCLK_CLK1 [get_bd_ports adc_base_clock] [get_bd_pins processing_system7_0/FCLK_CLK1]
   connect_bd_net -net processing_system7_0_FCLK_RESET0_N [get_bd_pins processing_system7_0/FCLK_RESET0_N] [get_bd_pins rst_ps7_0_200M/ext_reset_in]
   connect_bd_net -net random_1 [get_bd_ports random] [get_bd_pins spi_programmer_0/random]
   connect_bd_net -net rst_ps7_0_200M_peripheral_aresetn [get_bd_pins adc_signal_detector_0/m00_axis_aresetn] [get_bd_pins axi_dma_0/axi_resetn] [get_bd_pins axi_mem_intercon/ARESETN] [get_bd_pins axi_mem_intercon/M00_ARESETN] [get_bd_pins axi_mem_intercon/S00_ARESETN] [get_bd_pins gpio_enable_splitter/s_axi_aresetn] [get_bd_pins gpio_input_trigger/s_axi_aresetn] [get_bd_pins gpio_num_words/s_axi_aresetn] [get_bd_pins hold_signal_0/m00_axis_aresetn] [get_bd_pins ps7_0_axi_periph/ARESETN] [get_bd_pins ps7_0_axi_periph/M00_ARESETN] [get_bd_pins ps7_0_axi_periph/M01_ARESETN] [get_bd_pins ps7_0_axi_periph/M02_ARESETN] [get_bd_pins ps7_0_axi_periph/M03_ARESETN] [get_bd_pins ps7_0_axi_periph/S00_ARESETN] [get_bd_pins rst_ps7_0_200M/peripheral_aresetn] [get_bd_pins spi_programmer_0/reset] [get_bd_pins util_vector_logic_0/Op2]
