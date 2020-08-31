c4f5ea665f9ef32aba14dd88f3897fe0176839a8
Sushi Fish <sushi@sushitee.de>
2020-08-31 16:18:47 +0200

    changed version to dev again

--- cpp/simpleneutronclient/cpp/src/version.cpp
+++ cpp/simpleneutronclient/cpp/src/version.cpp
@@ -11,7 +11,7 @@ QObject *Version::instance(QQmlEngine *engine, QJSEngine *scriptEngine)
 
 QString Version::version() const
 {
-    return "v1.1.0";
+    return "v1.1.1 dev";
 }
 
 QString Version::buildTime() const
